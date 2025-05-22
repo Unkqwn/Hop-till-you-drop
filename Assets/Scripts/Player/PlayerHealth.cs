0f08e79797e7234c9dc387472bce06ecf76f56b8
Unkqwn <unlqwn@hotmail.com>
2025-05-22 09:45:28 +0200

    Health working with place holders

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -1,8 +1,29 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
     [SerializeField] private float health;
+
+    [SerializeField] private GameObject heartPrefab;
+    [SerializeField] private GameObject heartParent;
+    [SerializeField] private Sprite emptyHeart;
+    [SerializeField] private Sprite fullHeart;
+    [SerializeField] private List<Image> heartImage = new List<Image>();
+
+    private void Start()
+    {
+        if (health != null)
+        {
+            for (int i = 0; i < health; i++)
+            {
+                GameObject heart = Instantiate(heartPrefab, heartParent.transform);
+                heartImage.Add(heart.GetComponent<Image>());
+            }
+        }
+    }
+
     void Update()
     {
         if (health <= 0)
@@ -10,6 +31,7 @@ public class PlayerHealth : MonoBehaviour
             Time.timeScale = 0f;
             Destroy(this.gameObject);
         }
+        HeartsUpdate();
     }
 
     private void OnTriggerEnter(Collider other)
@@ -18,6 +40,21 @@ public class PlayerHealth : MonoBehaviour
         {
             health -= 1f;
             Destroy(other);
+        }
+    }
+
+    public void HeartsUpdate()
+    {
+        for (int i = 0; i < heartImage.Count; i++)
+        {
+            if (i < health)
+            {
+                heartImage[i].sprite = fullHeart;
+            }
+            else
+            {
+                heartImage[i].sprite = emptyHeart;
+            }
         }
     }
 }
